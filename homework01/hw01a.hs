812a22c57ad8772f1701f8128569ba8b61d8e089
Andrew O'Neill <aoneill@paytronix.com>
2014-11-05 11:36:31 -0500

    Combined two cases for 'toDigitsRev' on homework 1a

--- homework01/hw01a.hs
+++ homework01/hw01a.hs
@@ -18,9 +18,8 @@ reverseList (x:xs) = reverseList xs ++ [x]
 reverseList as = as
 
 toDigitsRev :: Integer -> [ Integer ]
-toDigitsRev 0 = []
 toDigitsRev n
-    | ( n < 0 ) = []
+    | ( n <= 0 ) = []
     | otherwise = ( n `mod` 10 ) : toDigitsRev ( n `div` 10 )
 
 
