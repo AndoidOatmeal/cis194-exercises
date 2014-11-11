b8346e4b97dd36828eb021497c7fbca62e44fc79
Andrew O'Neill <aoneill@paytronix.com>
2014-11-11 17:07:19 -0500

    Made some progress on hw02

--- homework02/LogAnalysis.hs
+++ homework02/LogAnalysis.hs
@@ -5,14 +5,8 @@
 -- module LogAnalysis where
 
 import Log
+import Text.Read
 
-
--- Exercise 1
-parseMessageContents :: String -> LogMessage
-
-parseMessage :: String -> LogMessage
-parseMessage logString = case logString of
-    ( 'I ':s ) -> LogMessage Info (-1) "stub"
 
 -- Simple assertion function for unit testing
 assert :: ( Show a, Eq a ) => String -> a -> a -> IO ()
@@ -21,11 +15,23 @@ assert message given expected
     | otherwise = fail ( "Test failed: " ++ message ++ ". Given: " ++ show given ++ " Expected: " ++ show expected )
 
 
+-- Exercise 1
+parseMessage :: String -> LogMessage
+parseMessage logString = case words logString of
+    ( "I" : timeStamp : content ) -> LogMessage Info ( read timeStamp ) $unwords content
+    ( "W" : timeStamp : content ) -> LogMessage Warning ( read timeStamp ) $unwords content
+    ( "E" : errorLevel : timeStamp : content ) -> LogMessage ( Error ( read errorLevel ) ) ( read timeStamp ) $unwords content
+    other -> Unknown logString
+
 -- Unit testing
 testAll :: IO ()
 testAll = do
     -- Exercise 1
     assert "Parse simple info log" ( parseMessage "I 29 la la la" ) $LogMessage Info 29 "la la la"
+    assert "Parse simple warning log" ( parseMessage "W 52 warning!" ) $LogMessage Warning 52 "warning!"
+    assert "Parse simple error log" ( parseMessage "E 94 25 there is an error!" ) $LogMessage ( Error 94 ) 25 "there is an error!"
+    assert "Parse simple invalid log" ( parseMessage "this is not a known log type" ) $Unknown "this is not a known log type"
+    assert "Parse an malformed info log" ( parseMessage "I la la la" ) $Unknown "I la la la"
 
 
 main = do
